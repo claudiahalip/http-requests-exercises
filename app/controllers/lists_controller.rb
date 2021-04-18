a916812ff6cf31902451e61eeaf96a10e4db144b
claudiahalip <claudia4ro@yahoo.com>
2021-04-18 14:52:11 -0500

    list controller actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,47 @@
 class ListsController < ApplicationController
+    def index
+        lists = List.all 
+        render json: lists 
+    end
+
+    def show
+        list = List.find(params[:id])
+        render json: list
+    end
+
+    def create
+       list = List.new(list_params)
+       if list.save 
+        render json:list
+       else
+        render json: {error: "The list couldn't be created.Please try again"}
+       end
+    end
+
+    def edit
+       list = List.find(params[:id])
+    end
+
+    def update
+        list = List.find(parmas[:id])
+        if list.upadate(list_params)
+            render json: list
+        else
+            render json: {error: "200"}
+        end
+    end
+
+    def destroy
+        list = List.find(params[:id])
+        list.destroy(
+            render json: {message: "The list was succesfully deleted."}
+        )
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:name, :status)
+       
+    end
 end
